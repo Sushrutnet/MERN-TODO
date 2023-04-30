543a62e448f0f98eb9cf3d37769fe370d7a6c6a8
Sushrutnet <sushrutnet@gmail.com>
2023-04-30 19:18:40 +0530

    first commit

---
+++ backend/Server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const routes =require('./routes/ToDoRoute')
+
+require('dotenv').config() 
+
+const app = express()
+const PORT = process.env.PORT || 4000
+
+mongoose
+.connect(process.env.MONGODB_URL)
+.then(()=>console.log(`Conneted To MongoDb...`))
+.catch((err)=>console.log(err))
+
+
+app.use(express.json())
+app.use(cors())
+
+app.use(routes)
+app.listen (PORT , () => console.log(`Listen on: ${PORT}`))
